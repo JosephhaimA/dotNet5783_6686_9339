0997e80f3d6894c61fcc7b1cef4cc000aeb13c71
amhai <amhaim964@gmail.com>
2022-10-31 00:48:19 +0200

    Code level zero task

---
+++ dotNet5783_6686_9339/Stage0/Program6686.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6686();
+            Welcome9339();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome9339();
+
+        private static void Welcome6686()
+        {
+            Console.Write("Enter your name ");
+            string usereName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", usereName);
+        }
+    }
+
+
+
+
+}
