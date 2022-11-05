c51a1072939dc17a503675f8478a0ad09f912bc0
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-05 23:32:43 +0200

    aa

--- dotNet5783_6686_9339/DalFacade/DO/Order.cs
+++ dotNet5783_6686_9339/DalFacade/DO/Order.cs
@@ -9,12 +9,15 @@ public struct Order
 {
     public int ID { get; set; }
     public string CostumerNmae { get; set; }
+    public string CostumerEmail { get; set; }
     public string CostumerAdress { get; set; }
     public DateTime OrderDate { get; set; }
     public DateTime shipDate { get; set; }
     public DateTime DeliveryrDate { get; set; }
     public override string ToString() => $@"
-
+order ID : {ID} 
+costumer name : {CostumerNmae} , costumer email : {CostumerEmail} , costumer adress {CostumerAdress}
+order date : {OrderDate} , ship date : {shipDate} , {DeliveryrDate}
 ";
 
 }
--- dotNet5783_6686_9339/DalFacade/DO/OrderItem.cs
+++ dotNet5783_6686_9339/DalFacade/DO/OrderItem.cs
@@ -7,5 +7,5 @@ public struct OrderItem
     public int ProductID { get; set; }
     public int OrderID { get; set; }
     public double Price { get; set; }
-    public int Amount { get; set; }
+    public int Amount { get; set; }kk
 }
