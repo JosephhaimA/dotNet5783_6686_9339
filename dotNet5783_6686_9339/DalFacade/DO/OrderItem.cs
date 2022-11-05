03e19e4766328c9ddb0a1f2121a6a85cc7c64e81
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-05 23:35:07 +0200

    aa

--- dotNet5783_6686_9339/DalFacade/DO/OrderItem.cs
+++ dotNet5783_6686_9339/DalFacade/DO/OrderItem.cs
@@ -7,5 +7,9 @@ public struct OrderItem
     public int ProductID { get; set; }
     public int OrderID { get; set; }
     public double Price { get; set; }
-    public int Amount { get; set; }kk
+    public int Amount { get; set; }
+    public override string ToString() => $@"
+prodect ID : {ProductID} ,  order ID : {OrderID} 
+price : {Price} , amount {Amount}
+";
 }
