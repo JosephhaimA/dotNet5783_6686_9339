59bed5898df139cde6be9a009900160bee8b8c21
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-06 00:58:26 +0200

    aa

---
+++ dotNet5783_6686_9339/DalList/DataSource .cs
@@ -0,0 +1,15 @@
+﻿
+using DO;
+
+namespace Dal;
+
+internal static class DataSource
+{
+   // public static readonly DataSource num = new DataSource();
+    static internal Order[] OrderArray = new Order[100];
+    static internal OrderItem[] OrderIteamArray = new OrderItem[200];
+    static internal Product[] ProductArray = new Product[50];
+
+    s_Initialize 
+
+}
