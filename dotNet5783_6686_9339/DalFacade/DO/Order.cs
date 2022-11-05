88a88fb1ef8f9ed77cc2f1f5fd278ca7958896f1
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-05 23:23:37 +0200

    aa

--- dotNet5783_6686_9339/DalFacade/DO/Order.cs
+++ dotNet5783_6686_9339/DalFacade/DO/Order.cs
@@ -1,7 +1,20 @@
 ﻿
 
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct Order
 {
+    public int ID { get; set; }
+    public string CostumerNmae { get; set; }
+    public string CostumerAdress { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime shipDate { get; set; }
+    public DateTime DeliveryrDate { get; set; }
+    public override string ToString() => $@"
+
+";
+
 }
