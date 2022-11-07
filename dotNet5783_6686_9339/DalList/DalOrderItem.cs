90ed93124ed32f55dbc1e8f50a7ea063c7c3e0e5
amhai <amhaim964@gmail.com>
2022-11-07 18:11:24 +0200

    dvdv

--- dotNet5783_6686_9339/DalList/DalOrderItem.cs
+++ dotNet5783_6686_9339/DalList/DalOrderItem.cs
@@ -3,4 +3,14 @@ namespace Dal;
 
 public class DalOrderItem
 {
+
+    public int AddOrder()
+    {
+
+        return 0;
+    }
+    public void ()
+    {
+
+    }
 }
--- dotNet5783_6686_9339/DalTest/Main.cs
+++ dotNet5783_6686_9339/DalTest/Main.cs
@@ -8,5 +8,6 @@ namespace Dal
 {
     internal class Main
     {
+
     }
 }
