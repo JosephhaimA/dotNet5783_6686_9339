b9132ecc97bdc6aab84dc561abf2e70b3ace5d9d
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-28 13:54:05 +0200

    a

--- dotNet5783_6686_9339/BL/BO/Exceptions.cs
+++ dotNet5783_6686_9339/BL/BO/Exceptions.cs
@@ -5,17 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BO;
-//public class DalDoesNotExistException : Exception
-//{
-//    public DalDoesNotExistException(string? message) : base(message) { }
-//}
-//public class DalAlreadyExistException : Exception
-//{
-//    public DalAlreadyExistException(string? message) : base(message) { }
-//}
+public class DalDoesNotExistException : Exception
+{
+    public DalDoesNotExistException(string? message) : base(message) { }
+}
+public class DalAlreadyExistException : Exception
+{
+    public DalAlreadyExistException(string? message) : base(message) { }
+}
 
-//public class DalDataCorruption : Exception
-//{
-//    public DalDataCorruption(string? message) : base(message) { }
-//}
+public class DalDataCorruption : Exception
+{
+    public DalDataCorruption(string? message) : base(message) { }
+}
 
