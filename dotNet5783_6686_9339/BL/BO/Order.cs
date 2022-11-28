5be0ae14289f60d31ac56c0097eaddb0edb1284f
amhai <amhaim964@gmail.com>
2022-11-28 13:22:07 +0200

    updTE 5

--- dotNet5783_6686_9339/BL/BO/Order.cs
+++ dotNet5783_6686_9339/BL/BO/Order.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DO;
 
 namespace BO;
-internal class Order
+public class Order
 {
     public int Id { get; set; }
     public string CostumerName { get; set; }
-    public string Email { get; set; }
+    public string Costumermail { get; set; }
+    public string CostumerAdress { get; set; }
+    public bool StatusOrder { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime ShipDate { get; set; }
+    public DateTime DeliveryDate { get; set; }
+    public List<OrderItem> Details {get; set; }
+    public float TotalPrice { get; set; }
 
+    public override string ToString() => $@"
+order ID : {Id} 
+costumer name : {CostumerName} 
+costumer email : {Costumermail} 
+costumer adress {CostumerAdress}
+order date : {OrderDate} 
+ship date : {ShipDate} 
+dalivery date : {DeliveryDate}
+";
 }
