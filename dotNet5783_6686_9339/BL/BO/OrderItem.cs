9b58e9bf3fd739a05b5f1115e73ecb0aa408423d
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-28 13:22:06 +0200

    uptade

---
+++ dotNet5783_6686_9339/BL/BO/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+public class OrderItem
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public string ProductName { get; set; }
+    public int ProductPrice { get; set; }
+    public int InOrder { get; set; }
+    public int SumPrice { get; set; }
+
+    public override string ToString() => 
+$@"
+order item ID : {Id} 
+order id : {OrderId} 
+product name: {ProductName} 
+product price {ProductPrice}
+how much in order : {InOrder} 
+sum price to this item : {SumPrice} 
+";
+}
+
