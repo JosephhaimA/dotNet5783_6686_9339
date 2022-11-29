712803077d7f34667708f3fb51a4a0369142bd1f
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-29 13:32:40 +0200

    update

---
+++ dotNet5783_6686_9339/BL/BlApi/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+namespace BlApi;
+public interface IProduct
+{
+    public IEnumerable<ProductForList> ListProduct();
+    public Product GetProductAdmin (int id);
+    public ProductItem GetProductAdminCostumer(int id, Cart cart);
+    public void ProductAdd(int id, string name, double price, ProductCategory category, int intStock);
+    public void ProductDelete (int id);
+    public void ProductUpdate (Product product);
+
+}
+
