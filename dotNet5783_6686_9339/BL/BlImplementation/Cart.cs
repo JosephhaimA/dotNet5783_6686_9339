5d47c0d80157db11551975b5683a77ee95c01ddd
אמיר חי תמר <amurhay@g.jct.ac.il>
2022-11-29 13:52:16 +0200

    updat

---
+++ dotNet5783_6686_9339/BL/BlImplementation/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+namespace BlImplementation;
+internal class Cart : ICart
+{
+    public Cart AddItemToCART(Cart item, int id) { return item; }
+    public Cart UpdateAmountOfProduct(Cart item, int id, int newAmount) { return item; }
+    public void ConfirmationOfOrder(Cart CostumerInfo) { }
+
+}
+
